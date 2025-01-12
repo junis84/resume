454104d5759badc93e619f78cb5fe2356265e0bd
Junyoung Eom <junyoung.eom@musinsa.com>
2025-01-12 18:12:00 +0900

    chore: re-generate docs

--- payload/etc.ts
+++ payload/etc.ts
@@ -20,11 +20,6 @@ const etc: IEtc.Payload = {
       startedAt: '2014-04',
     },
     {
-      title: '멘사 회원 가입',
-      subTitle: '멘사 테스트 통과 (IQ 148)',
-      startedAt: '2014-04',
-    },
-    {
       title: '육군 병장 만기 전역',
       subTitle: '육군교육사령부 헌병대 군수병',
       startedAt: '2003-11',
